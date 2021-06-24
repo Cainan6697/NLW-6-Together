1aa2e77387b7c2c64ca187a7503d44c7d5554e7f
Cainan <cainan6697@gmail.com>
2021-06-24 12:25:44 -0300

    Funcionalidade de encerrar sala na pag do admin + mensagem de que a sala foi encerrada caso alguem tente entrar numa sala que foi fechada

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg'
@@ -16,9 +16,18 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
+  const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const { questions, title } = useRoom(roomId);
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date()
+    })
+
+    history.push('/');
+  }
 
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm('Você tem certeza que deseja excluir essa pergunta?')) {
@@ -33,7 +42,7 @@ export function AdminRoom() {
           <img src={ logoImg } alt="Letmeask" />
           <div>
             <RoomCode code={ roomId } />
-            <Button isOutlined>Encerrar sala</Button>
+            <Button isOutlined onClick={ handleEndRoom }>Encerrar sala</Button>
           </div>
         </div>
       </header>

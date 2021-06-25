3afd34b9bca083cf68fa6f8bed0ed1f294572070
Cainan <cainan6697@gmail.com>
2021-06-25 09:03:47 -0300

    Add funcionalidade de marcar pergunta como respondida e marcar pergunta como destaque

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,7 +1,9 @@
 import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
-import deleteImg from '../assets/images/delete.svg'
+import deleteImg from '../assets/images/delete.svg';
+import answerImg from '../assets/images/answer.svg';
+import checkImg from '../assets/images/check.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -27,6 +29,18 @@ export function AdminRoom() {
     })
 
     history.push('/');
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighLightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   async function handleDeleteQuestion(questionId: string) {
@@ -60,7 +74,27 @@ export function AdminRoom() {
                   key={ question.id }
                   content={ question.content }
                   author={ question.author }
+                  isAnswered={ question.isAnswered }
+                  isHighlighted={ question.isHighlighted }
                 >
+                  {
+                    !question.isAnswered && (
+                      <>
+                        <button
+                          type="button"
+                          onClick={ () => handleCheckQuestionAsAnswered(question.id) }
+                        >
+                          <img src={ checkImg } alt="Marcar pergunta como respondida" />
+                        </button>
+                        <button
+                          type="button"
+                          onClick={ () => handleHighLightQuestion(question.id) }
+                        >
+                          <img src={ answerImg } alt="Dar destaque à pergunta que está sendo respondida" />
+                        </button>
+                      </>
+                    )
+                  }
                   <button
                     type="button"
                     onClick={ () => handleDeleteQuestion(question.id) }
